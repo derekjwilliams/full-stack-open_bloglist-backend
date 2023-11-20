d6a769dfb90636f2d6147c8e666aa7e82d2dca76
derek williams <derek61@gmail.com>
2023-11-20 15:33:25 -0700

    add delete test cd /Users/derekwilliams/development/tutorials/full-stack-open_bloglist-backend ; /usr/bin/env 'NODE_OPTIONS= --require /Applications/VSCodium.app/Contents/Resources/app/extensions/ms-vscode.js-debug/src/bootloader.js  --inspect-publish-uid=http' 'VSCODE_INSPECTOR_OPTIONS=:::{inspectorIpc:/var/folders/ph/0mt5s5d94zv3k16lhfcs19ym0000gn/T/node-cdp.97710-abb6d9be-13.sock,deferredMode:false,waitForDebugger:,execPath:/Users/derekwilliams/.nvm/versions/node/v20.5.0/bin/node,onlyEntrypoint:false,autoAttachMode:always,fileCallback:/var/folders/ph/0mt5s5d94zv3k16lhfcs19ym0000gn/T/node-debug-callback-f8ef11e79a70c19b}' /Users/derekwilliams/.nvm/versions/node/v20.5.0/bin/node /Users/derekwilliams/development/tutorials/full-stack-open_bloglist-backend/node_modules/.bin/jest --runInBand

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -10,75 +10,92 @@ const Blog = require('../models/blog')
 // Uses the test database, see utils/config.js
 beforeEach(async () => {
   await Blog.deleteMany({})
-
-  for (let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog)
-    await blogObject.save()
-  }
+  await Blog.insertMany(helper.initialBlogs)
 })
 
-test('a blog can be added', async () => {
-  const newBlog = helper.newBlog
+describe('get blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  }, 10000)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const title = blogsAtEnd.map((r) => r.title)
-  expect(title).toContain(newBlog.title)
+  test('blogs all contain an id property', async () => {
+    const response = await api.get('/api/blogs')
+    for (let blog of response.body) {
+      expect(blog.id).toBeDefined()
+    }
+  }, 10000)
 })
 
-test('a blog with a missing likes property is added with the likes property set to zero', async () => {
-  const newBlog = helper.newBlogMissingLikes
+describe('add blog', () => {
+  test('a blog can be added', async () => {
+    const newBlog = helper.newBlog
 
-  const postedBlog = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-  expect(postedBlog.body.likes).toEqual(0)
-}, 10000)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-test('a blog with a missing title property causes a 400 error', async () => {
-  const newBlog = helper.newBlogMissingTitle
+    const title = blogsAtEnd.map((r) => r.title)
+    expect(title).toContain(newBlog.title)
+  }, 10000)
 
-  const postedBlog = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-}, 10000)
+  test('a blog with a missing likes property is added with the likes property set to zero', async () => {
+    const newBlog = helper.newBlogMissingLikes
 
-test('a blog with a missing url property causes a 400 error', async () => {
-  const newBlog = helper.newBlogMissingUrl
+    const postedBlog = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const postedBlog = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-}, 10000)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    expect(postedBlog.body.likes).toEqual(0)
+  }, 10000)
 
+  test('a blog with a missing title property causes a 400 error', async () => {
+    const newBlog = helper.newBlogMissingTitle
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-}, 10000)
+    const postedBlog = await api.post('/api/blogs').send(newBlog).expect(400)
+  }, 10000)
 
-test('blogs all contain an id property', async () => {
-  const response = await api.get('/api/blogs')
-  for (let blog of response.body) {
-    expect(blog.id).toBeDefined()
-  }
-}, 10000)
+  test('a blog with a missing url property causes a 400 error', async () => {
+    const newBlog = helper.newBlogMissingUrl
+
+    const postedBlog = await api.post('/api/blogs').send(newBlog).expect(400)
+  }, 10000)
+})
+
+describe('delete blog', () => {
+  test('a blog can be deleted', async () => {
+    const startBlogs = await helper.blogsInDb()
+    const blog = startBlogs[0]
+  
+    await api
+      .delete(`/api/blogs/${blog.id}`)
+      .expect(204)
+  
+    const endBlogs = await helper.blogsInDb()
+  
+    expect(endBlogs).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+  
+    const titles = endBlogs.map(r => r.title)
+  
+    expect(titles).not.toContainEqual(blog.title)
+  })
+  
+})
+describe('update blog', () => {
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
